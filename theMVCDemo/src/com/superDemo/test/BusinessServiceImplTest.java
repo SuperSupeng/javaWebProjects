c74db43ffacbe2a2f0c74c35970383a15d92bdba
super <supeng842499467@gmail.com>
2018-01-18 21:06:59 +0800

    backgroundImpl
    后台实现

--- theMVCDemo/src/com/superDemo/test/BusinessServiceImplTest.java
+++ theMVCDemo/src/com/superDemo/test/BusinessServiceImplTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 
 import java.util.Date;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 public class BusinessServiceImplTest {
     private BusinessService s = new BusinessServiceImpl();
 
@@ -16,5 +19,20 @@ public class BusinessServiceImplTest {
         User user = new User("wzhting", "123", "wzt@itcast.cn", new Date());
         s.register(user);
     }
-    
+
+    @Test(expected = com.superDemo.execption.UserExistExecption.class)
+    public void testRegister1() throws UserExistExecption{
+        User user = new User("wzhting", "123", "wzt@itcast.cn", new Date());
+        s.register(user);
+    }
+
+    @Test
+    public void testlogin(){
+        User user = s.login("wzhting", "123");
+        assertNotNull(user);
+        user = s.login("wzhting", "111");
+        assertNull(user);
+        user = s.login("asf", "111");
+        assertNull(user);
+    }
 }
