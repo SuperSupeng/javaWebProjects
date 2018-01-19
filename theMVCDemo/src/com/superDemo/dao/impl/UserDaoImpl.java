4c3fedb7a95f831d7d98a53d9f44cd30ab3b3e32
super <supeng842499467@gmail.com>
2018-01-19 11:25:42 +0800

    cahngeToDataBase
    使用数据库

---
+++ theMVCDemo/src/com/superDemo/dao/impl/UserDaoImpl.java
@@ -0,0 +1,90 @@
+package com.superDemo.dao.impl;
+
+import com.superDemo.dao.UserDao;
+import com.superDemo.domain.User;
+import com.superDemo.util.JdbcUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public User finUserByUsername(String username) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try{
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement("select username,password,email,birthday from user where username=?");
+			//���ò���
+			stmt.setString(1, username);//��һ���ʺŵ�������1
+
+			rs = stmt.executeQuery();
+
+			if(rs.next()){
+				User user = new User();
+				user.setUsername(rs.getString("username"));
+				user.setPassword(rs.getString("password"));
+				user.setEmail(rs.getString("email"));
+				user.setBitrhday(rs.getDate("birthday"));
+				return user;
+			}
+			return null;
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}finally{
+			JdbcUtil.release(rs, stmt, conn);
+		}
+	}
+
+	public void save(User user) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try{
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement("insert into user (username,password,email,birthday) values (?,?,?,?)");
+			stmt.setString(1, user.getUsername());
+			stmt.setString(2, user.getPassword());
+			stmt.setString(3, user.getEmail());
+			stmt.setDate(4, new java.sql.Date(user.getBitrhday().getTime()));
+			
+			stmt.executeUpdate();
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}finally{
+			JdbcUtil.release(rs, stmt, conn);
+		}
+	}
+
+	public User findUser(String username, String password) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try{
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement("select username,password,email,birthday from user where username=? and password=?");
+			stmt.setString(1, username);
+			stmt.setString(2, password);
+			
+			rs = stmt.executeQuery();
+			
+			if(rs.next()){
+				User user = new User();
+				user.setUsername(rs.getString("username"));
+				user.setPassword(rs.getString("password"));
+				user.setEmail(rs.getString("email"));
+				user.setBitrhday(rs.getDate("birthday"));
+				return user;
+			}
+			return null;
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}finally{
+			JdbcUtil.release(rs, stmt, conn);
+		}
+	}
+
+}
