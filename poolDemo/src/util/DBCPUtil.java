12bb3f8ccd64fbb2e150f8fd11c2a0dc3a3743bf
super <supeng842499467@gmail.com>
2018-01-22 17:59:29 +0800

    DBCPDemo
    DBCP框架

--- poolDemo/src/util/DBCPUtil.java
+++ poolDemo/src/util/DBCPUtil.java
@@ -1,4 +1,36 @@
 package util;
 
+import org.apache.commons.dbcp.BasicDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBCPUtil {
+    private static DataSource dataSource;
+
+    static {
+        InputStream in = DBCPUtil.class.getClassLoader().getResourceAsStream("dbcpconfig.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(in);
+            dataSource = BasicDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DataSource getDataSource(){
+        return dataSource;
+    }
+
+    public static Connection getConnection(){
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
