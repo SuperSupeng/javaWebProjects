34fdb73294bcdc9a93f8f429f514bf329042cce5
super <supeng842499467@gmail.com>
2018-01-22 17:53:23 +0800

    ProxyPattern
    动态代理

--- poolDemo/src/cglib/Client.java
+++ poolDemo/src/cglib/Client.java
@@ -1,4 +1,38 @@
 package cglib;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
 public class Client {
+    public static void main(String[] args){
+        final SpringBrother sb = new SpringBrother();
+        SpringBrother proxy = (SpringBrother) Enhancer.create(SpringBrother.class, new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects,
+                                    MethodProxy methodProxy) throws Throwable {
+                if("sing".equals(method.getName())){
+                    //唱歌
+                    float money = (Float)objects[0];
+                    if(money>10000){
+                        method.invoke(sb, money/2);
+                    }
+                }
+                if("dance".equals(method.getName())){
+                    //唱歌
+                    float money = (Float)objects[0];
+                    if(money>20000){
+                        method.invoke(sb, money/2);
+                    }
+                }
+                return null;
+            }
+        });
+        System.out.println(proxy instanceof SpringBrother);
+        proxy.sing(100000);
+        proxy.sing(50000);
+    }
+
 }
--- poolDemo/src/cglib/SpringBrother.java
+++ poolDemo/src/cglib/SpringBrother.java
@@ -1,4 +1,12 @@
 package cglib;
 
 public class SpringBrother {
+
+    public void sing(float money) {
+        System.out.println("拿到钱："+money+"开唱");
+    }
+
+    public void dance(float money) {
+        System.out.println("拿到钱："+money+"开跳");
+    }
 }
