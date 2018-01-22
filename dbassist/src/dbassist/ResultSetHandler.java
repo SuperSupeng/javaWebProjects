08d5be1d0d0a651d60908ad34cb62db311f87419
super <supeng842499467@gmail.com>
2018-01-22 19:01:26 +0800

    MyDBFrame
    实现的数据库框架

---
+++ dbassist/src/dbassist/ResultSetHandler.java
@@ -0,0 +1,12 @@
+package dbassist;
+
+import java.sql.ResultSet;
+
+public interface ResultSetHandler {
+    /**
+     * 把结果中的数据封装到指定的对象中
+     * @param rs
+     * @return 封装了数据的对象
+     */
+    Object handle(ResultSet rs);
+}
