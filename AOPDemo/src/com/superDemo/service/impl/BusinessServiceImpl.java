b0caa8937533be7b17bd3db9f40f97a4b32d18af
super <supeng842499467@gmail.com>
2018-01-23 13:17:59 +0800

    theAOPDemo
    面向切片编程与事务处理

--- AOPDemo/src/com/superDemo/service/impl/BusinessServiceImpl.java
+++ AOPDemo/src/com/superDemo/service/impl/BusinessServiceImpl.java
@@ -2,12 +2,21 @@ package com.superDemo.service.impl;
 
 import com.superDemo.dao.AccountDao;
 import com.superDemo.dao.impl.AccountDaoImpl;
+import com.superDemo.domain.Account;
 import com.superDemo.service.BusinessService;
 
+/**
+ * 只有业务层空集具体逻辑
+ */
 public class BusinessServiceImpl implements BusinessService{
     private AccountDao dao = new AccountDaoImpl();
     @Override
     public void transfer(String sourceAccountName, String targetAccountName, float money) {
-
+        Account sAccount = dao.findByName(sourceAccountName);
+        Account tAccount = dao.findByName(targetAccountName);
+        sAccount.setMoney(sAccount.getMoney() - money);
+        tAccount.setMoney(tAccount.getMoney() + money);
+        dao.update(sAccount);
+        dao.update(tAccount);
     }
 }
