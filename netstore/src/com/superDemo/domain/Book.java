dcb8624b253c09fb6c6d1f80e369fba78ddb0edd
super <supeng842499467@gmail.com>
2018-01-31 12:48:32 +0800

    book
    book

---
+++ netstore/src/com/superDemo/domain/Book.java
@@ -0,0 +1,78 @@
+package com.superDemo.domain;
+
+import java.io.Serializable;
+
+public class Book implements Serializable{
+    private String id;
+    private String name;
+    private String author;
+    private float price;
+    private String path;//存放图片的路径
+    private String photoFileName;//图片的文件名
+    private String description;
+    private String categoryId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getPhotoFileName() {
+        return photoFileName;
+    }
+
+    public void setPhotoFileName(String photoFileName) {
+        this.photoFileName = photoFileName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+}
