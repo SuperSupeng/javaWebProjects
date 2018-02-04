bdbde5e47eaf253142ad176d93cae479c3891395
super <supeng842499467@gmail.com>
2018-02-04 10:54:00 +0800

    email
    sendEmail

---
+++ netstore/src/com/superDemo/util/SendMail.java
@@ -0,0 +1,44 @@
+package com.superDemo.util;
+
+import com.superDemo.domain.Customer;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendMail extends Thread{
+    private Customer c;
+
+    public SendMail(Customer c){
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        try{
+            Properties props = new Properties();
+            props.setProperty("mail.transport.protocol", "smtp");//指定邮件发送的协议，参数是规范规定的
+            props.setProperty("mail.host", "smtp.163.com");//指定发件服务器的地址，参数是规范规定的
+//		    props.setProperty("mail.debug", "true");//邮件发送的调试模式，参数是规范规定的
+            props.setProperty("mail.smtp.auth", "true");//请求服务器进行身份认证。参数与具体的JavaMail实现有关
+            Session session = Session.getInstance(props);
+            MimeMessage message = new MimeMessage(session);
+
+            message.setFrom(new InternetAddress("itheima14@163.com"));
+            message.setRecipients(Message.RecipientType.TO, c.getEmail());
+            message.setSubject("来自XX网站的激活邮件");
+
+            message.setContent("亲爱的"+c.getUsername()+"<br/>感谢您注册成为我们的会员，请猛戳下面激活您的账户。<br/><a href='http://localhost:8080/day23_00_netstore/servlet/ClientServlet?op=active&code="+c.getCode()+"'>戳这里</a><br/>本邮件由系统自动发出，请不要直接回复。", "text/html;charset=UTF-8");
+            message.saveChanges();
+
+            Transport ts = session.getTransport();
+            ts.connect("itheima14", "iamsorry");
+            ts.sendMessage(message, message.getAllRecipients());
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- netstore/src/com/superDemo/web/controller/ClientServlet.java
+++ netstore/src/com/superDemo/web/controller/ClientServlet.java
@@ -3,8 +3,11 @@ package com.superDemo.web.controller;
 import com.superDemo.commons.Page;
 import com.superDemo.domain.Book;
 import com.superDemo.domain.Category;
+import com.superDemo.domain.Customer;
 import com.superDemo.service.BusinessService;
 import com.superDemo.service.impl.BusinessServiceImpl;
+import com.superDemo.util.FillBeanUtil;
+import com.superDemo.util.SendMail;
 import com.superDemo.web.beans.Cart;
 import com.superDemo.web.beans.CartItem;
 
@@ -16,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @WebServlet(name = "ClientServlet", urlPatterns = "/ClientServlet")
 public class ClientServlet extends HttpServlet {
@@ -38,7 +42,22 @@ public class ClientServlet extends HttpServlet {
             changeNum(request,response);
         }else if("delOneItem".equals(op)){
             delOneItem(request,response);
+        }else if("registCustomer".equals(op)){
+            registCustomer(request, response);
         }
+    }
+
+    //新用户注册，并发送激活邮件
+    private void registCustomer(HttpServletRequest request,
+                                HttpServletResponse response)  throws ServletException, IOException {
+        Customer c = FillBeanUtil.fillBean(request, Customer.class);
+        String code = UUID.randomUUID().toString();
+        c.setCode(code);
+        SendMail sm = new SendMail(c);
+        sm.start();
+        s.regitsCustomer(c);
+        request.setAttribute("msg", "注册成功！我们已经发送了一封激活邮件到您的"+c.getEmail()+"邮箱中，请及时激活您的账户");
+        request.getRequestDispatcher("/message.jsp").forward(request, response);
     }
 
     private void delOneItem(HttpServletRequest request,
