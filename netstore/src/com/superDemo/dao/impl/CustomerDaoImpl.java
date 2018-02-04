014da0f315d32ae4d2698c91a795ca709a70a48a
super <supeng842499467@gmail.com>
2018-02-04 10:40:28 +0800

    customerDao
    customerDao

--- netstore/src/com/superDemo/dao/impl/CustomerDaoImpl.java
+++ netstore/src/com/superDemo/dao/impl/CustomerDaoImpl.java
@@ -2,30 +2,66 @@ package com.superDemo.dao.impl;
 
 import com.superDemo.dao.CustomerDao;
 import com.superDemo.domain.Customer;
+import com.superDemo.exception.DaoException;
+import com.superDemo.util.DBCPUtil;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 
 public class CustomerDaoImpl implements CustomerDao {
-    @Override
+    private QueryRunner qr = new QueryRunner(DBCPUtil.getDataSource());
     public void save(Customer c) {
-        
+        try {
+            qr.update("insert into customers (id,username,password,phone,address,email,actived,code) values (?,?,?,?,?,?,?,?)",
+                    c.getId(),
+                    c.getUsername(),
+                    c.getPassword(),
+                    c.getPhone(),
+                    c.getAddress(),
+                    c.getEmail(),
+                    c.isActived(),
+                    c.getCode());
+        } catch (Exception e) {
+            throw new DaoException(e);
+        }
     }
 
-    @Override
-    public void update(Customer customer) {
-
-    }
-
-    @Override
     public Customer findByCode(String code) {
-        return null;
+        try {
+            return qr.query("select * from customers where code=?", new BeanHandler<Customer>(Customer.class),code);
+        } catch (Exception e) {
+            throw new DaoException(e);
+        }
     }
 
-    @Override
+    public void update(Customer c) {
+        try {
+            qr.update("update customers set username=?,password=?,phone=?,address=?,email=?,actived=?,code=? where id=?",
+                    c.getUsername(),
+                    c.getPassword(),
+                    c.getPhone(),
+                    c.getAddress(),
+                    c.getEmail(),
+                    c.isActived(),
+                    c.getCode(),
+                    c.getId());
+        } catch (Exception e) {
+            throw new DaoException(e);
+        }
+    }
+
     public Customer findCustomer(String username, String password) {
-        return null;
+        try {
+            return qr.query("select * from customers where username=? and password=?", new BeanHandler<Customer>(Customer.class),username,password);
+        } catch (Exception e) {
+            throw new DaoException(e);
+        }
     }
 
-    @Override
     public Customer findById(String customerId) {
-        return null;
+        try {
+            return qr.query("select * from customers where id=?", new BeanHandler<Customer>(Customer.class),customerId);
+        } catch (Exception e) {
+            throw new DaoException(e);
+        }
     }
 }
