ff8b7b51fc9412ba5f54e203844601cf614f92f1
super <supeng842499467@gmail.com>
2018-02-04 19:31:20 +0800

    schoolLesson
    schoolLesson

--- schoolLesson/src/domain/User.java
+++ schoolLesson/src/domain/User.java
@@ -8,8 +8,61 @@ public class User implements Serializable{
     private String id;
     private String name;
     private String password;
-
+    private String sex;
+    private int age;
+    private String qq;
+    private String telephone;
+    private String  email;
+    private String address;
     List<Frinends> frinends = new ArrayList<>();
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public List<Frinends> getFrinends() {
         return frinends;
--- schoolLesson/src/service/BusinessService.java
+++ schoolLesson/src/service/BusinessService.java
@@ -1,5 +1,22 @@
 package service;
 
+import domain.User;
+import exception.UserAlreadyExistedException;
+
 public interface BusinessService {
 
+     /**
+     * 用户注册
+     * @param user 注册的用户信息
+     * @throws UserAlreadyExistedException 若用户已存在则不允许注册
+     */
+    void register(User user) throws UserAlreadyExistedException;
+
+    /**
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    User login(String username, String password);
 }
