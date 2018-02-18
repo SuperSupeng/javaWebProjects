136e6e230b3b8d6a1412ccec4549e5e627ce53e0
super <supeng842499467@gmail.com>
2018-02-18 21:48:54 +0800

    addUser
    addUser

--- struts2Grid/src/actions/UserAction.java
+++ struts2Grid/src/actions/UserAction.java
@@ -3,10 +3,15 @@ package actions;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import domain.User;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.util.TokenHelper;
 import service.BusinessService;
 import service.impl.BusinessServiceImpl;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +21,11 @@ public class UserAction extends ActionSupport implements ModelDriven<User> {
     private BusinessService s = new BusinessServiceImpl();
 
     private String isUpload;
+    private String[] interests;
+
+    private File upload;
+
+    private String uploadFileName;
 
     public String userLogin() {
         user = s.login(user.getLogonName(), user.getLogonPwd());
@@ -34,6 +44,32 @@ public class UserAction extends ActionSupport implements ModelDriven<User> {
 
     public String listByCondition(){
         users = s.findUsersByCondition(user.getUserName(), user.getSex(), user.getEducation(), isUpload);
+        return SUCCESS;
+    }
+
+    public String addUser() throws IOException {
+        if(interests!=null&&interests.length>0){
+            StringBuffer sb = new StringBuffer();
+            for(int i=0;i<interests.length;i++){
+                if(i>0){
+                    sb.append(",");
+                }
+                sb.append(interests[i]);
+            }
+            user.setInterest(sb.toString());
+        }
+        if(upload!=null){
+            String realPath = ServletActionContext.getRequest().getRealPath("/files");
+            File storeDirectory = new File(realPath);
+            if(!storeDirectory.exists()){
+                storeDirectory.mkdirs();
+            }
+            user.setPath("/files");
+            String newFileName = TokenHelper.generateGUID()+"."+ FilenameUtils.getExtension(uploadFileName);
+            user.setFilename(newFileName);
+            FileUtils.copyFile(upload, new File(storeDirectory, newFileName));
+        }
+        s.addUser(user);
         return SUCCESS;
     }
 
@@ -59,6 +95,30 @@ public class UserAction extends ActionSupport implements ModelDriven<User> {
 
     public void setIsUpload(String isUpload) {
         this.isUpload = isUpload;
+    }
+
+    public String[] getInterests() {
+        return interests;
+    }
+
+    public void setInterests(String[] interests) {
+        this.interests = interests;
+    }
+
+    public File getUpload() {
+        return upload;
+    }
+
+    public void setUpload(File upload) {
+        this.upload = upload;
+    }
+
+    public String getUploadFileName() {
+        return uploadFileName;
+    }
+
+    public void setUploadFileName(String uploadFileName) {
+        this.uploadFileName = uploadFileName;
     }
 
     @Override
