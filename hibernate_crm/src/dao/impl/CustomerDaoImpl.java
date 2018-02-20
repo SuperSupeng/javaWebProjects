ca989541380ad5a2c9ad5edc447b3eac433025ad
super <supeng842499467@gmail.com>
2018-02-20 21:00:53 +0800

    transaction
    transaction

--- hibernate_crm/src/dao/impl/CustomerDaoImpl.java
+++ hibernate_crm/src/dao/impl/CustomerDaoImpl.java
@@ -3,16 +3,12 @@ package dao.impl;
 import dao.CustomerDao;
 import domain.Customer;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import utils.HibernateUtils;
 
 public class CustomerDaoImpl implements CustomerDao {
     @Override
     public void save(Customer c) {
-        Session session = HibernateUtils.openSession();
-        Transaction tx = session.beginTransaction();
+        Session session = HibernateUtils.getCurrentSession();
         session.save(c);
-        tx.commit();
-        HibernateUtils.release();
     }
 }
--- hibernate_crm/src/service/impl/BusinessServiceImpl.java
+++ hibernate_crm/src/service/impl/BusinessServiceImpl.java
@@ -3,13 +3,24 @@ package service.impl;
 import dao.CustomerDao;
 import dao.impl.CustomerDaoImpl;
 import domain.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import service.BusinessService;
+import utils.HibernateUtils;
 
 public class BusinessServiceImpl implements BusinessService {
     private CustomerDao dao = new CustomerDaoImpl();
 
     @Override
     public void save(Customer c) {
-        dao.save(c);
+        Session session = HibernateUtils.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        try{
+            dao.save(c);
+        }catch (Exception e){
+            e.printStackTrace();
+            tx.rollback();
+        }
+        tx.commit();
     }
 }
