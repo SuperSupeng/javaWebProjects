28a0227fb748cac3756afd38a7cdff89ea45daab
super <supeng842499467@gmail.com>
2018-02-28 19:37:59 +0800

    hibernate
    hibernate

--- hibernate_crm/src/dao/impl/CustomerDaoImpl.java
+++ hibernate_crm/src/dao/impl/CustomerDaoImpl.java
@@ -4,6 +4,7 @@ import dao.CustomerDao;
 import domain.Customer;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.DetachedCriteria;
 import utils.HibernateUtils;
 
 import java.util.List;
@@ -26,5 +27,15 @@ public class CustomerDaoImpl implements CustomerDao {
     public Customer getById(Long cust_id) {
         Session session = HibernateUtils.getCurrentSession();
         return session.get(Customer.class, cust_id);
+    }
+
+    @Override
+    public List<Customer> getAll(DetachedCriteria dc) {
+        //1 获得session
+        Session session = HibernateUtils.getCurrentSession();
+        //2 将离线对象关联到session
+        Criteria c = dc.getExecutableCriteria(session);
+        //3 执行查询并返回
+        return c.list();
     }
 }
