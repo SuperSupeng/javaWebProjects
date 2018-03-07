4b2732689b3d481224c3ec93c6af5b3c304d2d0e
super <supeng842499467@gmail.com>
2018-03-07 23:08:46 +0800

    iotInformation
    iotInformation

--- iotInformation/src/dao/impl/TempDataDaoImpl.java
+++ iotInformation/src/dao/impl/TempDataDaoImpl.java
@@ -3,6 +3,7 @@ package dao.impl;
 import dao.TempDataDao;
 import domain.TempData;
 import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import util.HibernateUtils;
@@ -36,6 +37,11 @@ public class TempDataDaoImpl implements TempDataDao {
 
     @Override
     public List<TempData> findAllData(Date startDate, Date endDate, boolean isInverse) {
+        Session session = HibernateUtils.getCurrentSession();
+        String sql = "select * from tempt where dataDate>=? and dataDate<=?";
+        SQLQuery query = session.createSQLQuery(sql);
+        query.setParameter(0, startDate);
+        query.setParameter(1, endDate);
         return null;
     }
 }
