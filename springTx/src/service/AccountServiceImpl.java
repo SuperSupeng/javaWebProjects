940ae373eeca98e949e3784a786e43741956e071
super <supeng842499467@gmail.com>
2018-03-08 11:12:31 +0800

    spring
    spring

--- springTx/src/service/AccountServiceImpl.java
+++ springTx/src/service/AccountServiceImpl.java
@@ -9,7 +9,6 @@ public class AccountServiceImpl implements AccountService {
     public void transfer(Integer from, Integer to, Double money) {
         //减钱
         ad.decreaseMoney(from, money);
-        int i = 1 / 0;
         //加钱
         ad.increaseMoney(to, money);
     }
