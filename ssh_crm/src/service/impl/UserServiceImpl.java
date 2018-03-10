1a1abddd371d569afcb832d1c98d7fe8839861f5
super <supeng842499467@gmail.com>
2018-03-10 09:35:33 +0800

    ssh
    ssh

---
+++ ssh_crm/src/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package service.impl;
+
+import domain.User;
+import service.UserService;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public User getUserByCodePassword(User u) {
+        System.out.println("getUserByCodePassword");
+        return null;
+    }
+}
--- ssh_crm/src/web/action/UserAction.java
+++ ssh_crm/src/web/action/UserAction.java
@@ -1,7 +1,15 @@
 package web.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import service.UserService;
 
+/**
+ * @author super
+ */
 public class UserAction extends ActionSupport{
+    private UserService us;
 
+    public void setUs(UserService us) {
+        this.us = us;
+    }
 }
