9a91602c6095dc5516bf99163adb6d68ce6852e7
super <supeng842499467@gmail.com>
2018-03-16 18:10:04 +0800

    iotInformation
    iotInformation

--- iotInformation/src/service/TempDataService.java
+++ iotInformation/src/service/TempDataService.java
@@ -5,14 +5,39 @@ import domain.TempData;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * @author super
+ */
 public interface TempDataService {
 
+    /**
+     * 保存
+     * @param tempData
+     */
     void save(TempData tempData);
 
+    /**
+     * 根据主键查询
+     * @param id
+     * @return
+     */
     TempData findById(String id);
 
 
+    /**
+     * 查询所有数据
+     * @param startDate
+     * @param endDate
+     * @param isInverse
+     * @return
+     */
     List<TempData> findAllData(Date startDate, Date endDate, boolean isInverse);
 
+    /**
+     * 查询所有数据
+     * @param startDate
+     * @param endDate
+     * @return
+     */
     List<TempData> findAllData(Date startDate, Date endDate);
 }
